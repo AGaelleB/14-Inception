4054eac43e96f403f80361382702371c5cfc2dd7
abonnefo <abonnefo@VMInception.abonnefo.42.fr>
2024-07-24 14:30:22 +0200

    Docker nginx ok mais pb avec wordpress

---
+++ lili.c
@@ -0,0 +1,9 @@
+srcs/
+├── requirements/
+│   ├── nginx/
+│   │   ├── Dockerfile
+│   │   ├── nginx.conf
+│   │   └── ssl/
+│   │       ├── nginx.crt
+│   │       └── nginx.key
+└── docker-compose.yml
