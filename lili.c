2ea017e4650ab3126a979016a77d039a94b1140c
abonnefo <abonnefo@VMInception.abonnefo.42.fr>
2024-07-25 11:54:30 +0200

    Docker nginx pb to fix, mais port + TSLv1.2 OK

--- lili.c
+++ lili.c
@@ -1,9 +1,6 @@
-srcs/
-├── requirements/
-│   ├── nginx/
-│   │   ├── Dockerfile
-│   │   ├── nginx.conf
-│   │   └── ssl/
-│   │       ├── nginx.crt
-│   │       └── nginx.key
-└── docker-compose.yml
+maintenant je veux adapter wordpress
+
+A Docker container that contains WordPress + php-fpm (it must be installed and
+configured) only without nginx.
+A volume that contains your WordPress database.
+A second volume that contains your WordPress website files.
