c0b7b3eab2102240f12f5cb3f31abaf8ddca17f2
abonnefo <annegaelle.bonnefoy@gmail.con>
2024-08-27 15:30:22 +0200

    mariadb mysql acces au docker OK

--- srcs/requirements/mariadb/tools/init.sql
+++ srcs/requirements/mariadb/tools/init.sql
@@ -1,15 +1,15 @@
 -- Créer la base de données si elle n'existe pas
 CREATE DATABASE IF NOT EXISTS `DBwordpress`;
 
--- Créer un utilisateur avec un mot de passe
+-- Créer un utilisateur avec un mdp
 CREATE USER IF NOT EXISTS 'wpUser'@'%' IDENTIFIED BY '123';
 
--- Accorder tous les privilèges à cet utilisateur
+-- Accorder tous les privilèges
 GRANT ALL PRIVILEGES ON `DBwordpress`.* TO 'wpUser'@'%';
 
--- Configurer le mot de passe root et accorder les privilèges
+-- Configurer le mdp root et accorder les privilèges
 ALTER USER 'root'@'localhost' IDENTIFIED BY 'DBroot';
 GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
 
--- Appliquer les modifications
+-- Appliquer les modif
 FLUSH PRIVILEGES;
