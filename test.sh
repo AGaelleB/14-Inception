c6ca951f8c2ad4ee8b6e83386bee9b5e0599e2a0
abonnefo <abonnefo@student.42.fr>
2024-09-01 16:00:21 +0200

    voir pour css

--- test.sh
+++ test.sh
@@ -1,25 +1,4 @@
 verifier les variables d environnement soient utilisees
 
 ---
-
-voir si j utilise 
-
-MARIADB_DATA_PATH=/home/abonnefo/data/mariadb
-WORDPRESS_DATA_PATH=/home/abonnefo/data/wordpress
-
-volumes:
-  mariadb_data:
-    driver: local
-    driver_opts:
-      type: none
-      device: ${MARIADB_DATA_PATH}
-      o: bind
-  wordpress_data:
-    driver: local
-    driver_opts:
-      type: none
-      device: ${WORDPRESS_DATA_PATH}
-      o: bind
-
----
-
+docker stop $(docker ps -qa); docker rm $(docker ps -qa); docker rmi -f $(docker images -qa); docker volume rm $(docker volume ls -q); docker network rm $(docker network ls -q) 2&gt;/dev/null
